3186df08949f0f71a9daa95272882d7291092150
Willem Danny <willemdanny@gmail.com>
2019-12-03 17:36:10 +0100

    fix wrong copy/paste from codewars.com

--- c#/Battleship field validator.cs
+++ c#/Battleship field validator.cs
@@ -67,32 +67,3 @@ namespace Solution {
       }
   }
 }
-6 days agoRefactorDiscuss
-5 kyu
-Integers: Recreation One
-C#:
-using System;
-using System.Collections.Generic;
-using System.Linq;
-
-public class SumSquaredDivisors 
-{
-	public static string listSquared(long m, long n)
-	{
-        Dictionary<long, long> results = new Dictionary<long, long>();
-        for(long i = m; i <= n; i++){
-            var dividers = FindDividers(i);
-            var sum = (long)dividers.Select(x=> Math.Pow(x, 2)).Sum();
-            if(Math.Sqrt(sum)%1 == 0){
-                results.Add(i, sum);
-            }
-        }
-        return $"[{string.Join(", ",results.Select(x=> $"[{x.Key}, {x.Value}]"))}]";
-	}
-
-    public static IEnumerable<long> FindDividers(long m){
-        for(long d = 1; d <= m; d++){
-            if(m%d == 0) yield return d;
-        }
-    }
-}
